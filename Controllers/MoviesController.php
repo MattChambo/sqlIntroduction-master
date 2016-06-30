62d586d463069291ed5afb97e368984ceb1077df
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-07-01 11:19:40 +1200

    Update movie controller

--- Controllers/MoviesController.php
+++ Controllers/MoviesController.php
@@ -7,8 +7,9 @@ require "Views/MovieFormView.php";
 class MoviesController extends Controller {
 
 	public function show() {
-		$movie = new Movie;
-		$singlemovie = $movie->find();
+
+		$id = isset($_GET['id']) ? ($_GET['id']) : null;
+		$singlemovie = new Movie($id);
 
 		$view = new MoviesView(compact('singlemovie'));
 		$view->render();
